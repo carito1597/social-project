1075e32927e66c37e8aaa4781e60a920666c09ff
Carolina Villalobos <carito15villalobos@gmail.com>
2022-12-09 22:56:29 -0400

    create components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ReportListComponent } from './report-list/report-list.component';
 import { ReportRegisterComponent } from './report-register/report-register.component';
 
 const routes: Routes = [
@@ -11,6 +13,14 @@ const routes: Routes = [
   {
     path: 'report-register',
     component: ReportRegisterComponent
+  },
+  {
+    path: 'report-list',
+    component: ReportListComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReportRegisterComponent } from './report-register/report-register.component';
-import { GoogleMapsModule } from '@angular/google-maps'
+import { GoogleMapsModule } from '@angular/google-maps';
+import { ContactComponent } from './contact/contact.component';
+import { ReportListComponent } from './report-list/report-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReportRegisterComponent
+    ReportRegisterComponent,
+    ContactComponent,
+    ReportListComponent
   ],
   imports: [
     BrowserModule,
